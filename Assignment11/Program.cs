6178a7719f90a341baa0c8300260e571cb8b5977
Philo <philoatefdev@gmail.com>
2024-07-25 16:06:55 +0300

    first question

--- Assignment11/Program.cs
+++ Assignment11/Program.cs
@@ -2,9 +2,44 @@
 {
     internal class Program
     {
+        //We can optimize a bubble sort by skipping the sorting if no swaps were made during the array passes.
+        //also if there is no swapping in a pass, then the array was sorted and we can skip the remaining passes.
+
+        public static void BubbleSortOptimized<T>(T[] array) where T : IComparable<T>
+        {
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isSwapped = false;
+                for (int j = 0; j < array.Length-i-1; j++)
+                {
+                    if (array[j].CompareTo(array[j+1]) > 0)
+                    {
+                        isSwapped = true;
+                        T temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+                if (!isSwapped) 
+                {
+                    break;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+            int[] arr = { 9, 8, 7, 12, 25, 77, 2 };
+            BubbleSortOptimized(arr);
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+
+
         }
     }
 }
